0f0057fbf6126cf0d2fe0a9fb79aadad19fefb39
Alyssa Levitz <alyssal@yammer-inc.com>
2013-09-14 13:06:28 -0700

    deleted the second devise_create_users migration - don't know why there were two - and also moved the n the noticealert thingies so they wouldn't be hidden by the nav

--- intermediate/db/schema.rb
+++ intermediate/db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130824213051) do
-
+ActiveRecord::Schema.define(version: 20130824202853) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
