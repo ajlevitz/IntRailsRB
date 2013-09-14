ca6bbc90427e0c0bd86470471f4cde2977172d39
Alyssa Levitz <alyssal@yammer-inc.com>
2013-09-14 15:05:29 -0700

    added a basic users controller so that i can list all users, get the list of posts by user, etc.

--- intermediate/config/routes.rb
+++ intermediate/config/routes.rb
@@ -12,6 +12,7 @@ Intermediate::Application.routes.draw do
 
   # to have all the restful blahblah
   resources :posts
+  resources :users
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
